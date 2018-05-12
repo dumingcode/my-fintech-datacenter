890a381f4f7f29955575cbacf497f593a8b9cde8
jake1036 <jake1036@126.com>
2018-05-12 18:41:37 +0800

    update unit test

--- test/tasktest.js
+++ test/tasktest.js
@@ -3,12 +3,23 @@ const expect = chai.expect
 
 const lxrIndexService = require('../service/lxrIndexService')
 const config = require('../config/config')
-
-describe('#lxrStockIndexTask()-验证理性人指数数据是否抓取成功，数据是否存储到redis中', function() {
-    it('lxrStockIndexTask ret right data',  function() {
-        return lxrIndexService.lauchLxrIndexTask().then((indexData)=>{
+describe('#lxr', () => {
+it('#lxrStockIndexTask ret right data', function (done) {
+    (async function () {
+        try {
+            let indexData = await lxrIndexService.lauchLxrIndexTask()
             expect(indexData.status).to.be.equal(200)
             expect(indexData.message).to.be.equal('OK')
-        })
-    }); 
-});
+            done()
+        } catch (err) {
+            if (err) throw err;
+        }
+    })()
+    
+    
+})
+})
+
+
+
+
