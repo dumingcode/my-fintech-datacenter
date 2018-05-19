50f6ce5ea0eb226b5327fe7b43ff91da3175c6ba
dumingcode <jake1036@126.com>
2018-05-20 05:39:30 +0800

    commit

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -4,7 +4,7 @@ const log = require('../util/logUtil')
 const logUtil = log.logUtil
 
 /**理性人指数数据自动处理任务 */
-schedule.scheduleJob('40 5-10 * * *', lxrStockIndexTask.lauchLxrIndexTask().then((val) => {
+schedule.scheduleJob('44 5-10 * * *', lxrStockIndexTask.lauchLxrIndexTask().then((val) => {
     logUtil.info({ val }, 'lauchLxrIndexTask success')
 }).catch((err) => {
     logUtil.error(err)
