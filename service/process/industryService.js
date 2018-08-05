41a43a63756981f477fddc1f130bf1a6536c3c26
dumingcode <jake1026@126.com>
2018-08-05 11:35:54 +0800

    补充股票名称

--- service/process/industryService.js
+++ service/process/industryService.js
@@ -9,9 +9,10 @@ const fs = require('fs');
 module.exports = {
     async launchIndustryTask() {
         console.log('start industry once time')
-        await this.handleStep1()
-        await this.handleStep2()
-        await this.handleStep3()
+            // await this.handleStep1()
+            // await this.handleStep2()
+            // await this.handleStep3()
+        await this.handleStep4()
         return { status: 200, message: 'OK' }
     },
     handleStep1() {
@@ -71,7 +72,43 @@ module.exports = {
                 console.log(val)
             })
         })
+    },
+
+    /**
+     * 处理代码跟股票名称
+     */
+    handleStep4() {
+        const rl = readline.createInterface({
+            input: fs.createReadStream('/tmp/2J.txt'),
+            crlfDelay: 100
+        });
+        //处理一级二级行业
+        rl.on('line', (line) => {
+            console.log(`Line from file: ${line}`)
+            line = line.replace(/\"/g, "")
+            let strs = line.split(',')
+            let code = strs[0].substring(0, 6)
+            let name = strs[1]
+            if (code == "00000") {
+                code = '000001'
+            }
+            //let stockInfo = { 'code': code, 'citiV1': citiV1, 'citiV2': citiV2 }
+            redisUtil.redisHGet(config.redisStoreKey.xueQiuStockSet, code).then((stockJson) => {
+                let stockInfo = { 'code': code, 'name': name }
+                if (stockJson) {
+                    stockInfo = JSON.parse(stockJson)
+                    stockInfo['name'] = name
+                }
+                return stockInfo
+            }).then((stockInfo) => {
+                return redisUtil.redisHSet(config.redisStoreKey.xueQiuStockSet, code, JSON.stringify(stockInfo))
+            }).then((val) => { console.log(val) }).catch((val) => {
+                console.log(val)
+            })
+        })
+
     }
+
 
 
 }
