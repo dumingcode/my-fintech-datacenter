e9954939d4d96072adb7f930d184b5d9589eecbc
dumingcode <jake1036@126.com>
2019-01-28 16:07:30 +0800

    完成个股相对沪深300的alpha&beta计算

--- bin/runOneStockHisData.js
+++ bin/runOneStockHisData.js
@@ -4,7 +4,7 @@ const stockHisDataService = require('../service/stockOneStockHisDataService')
 const log = require('../util/logUtil')
 const logUtil = log.logUtil
 /**一次性抓取全部个股640个交易日内的交易价格数据 */
-stockHisDataService.launchStockHisDataTask('600030').then((val) => {
+stockHisDataService.launchStockHisDataTask('600036').then((val) => {
     logUtil.info({ val }, 'launchStockHisDataTask success')
 }).catch((err) => {
     logUtil.error(err)
