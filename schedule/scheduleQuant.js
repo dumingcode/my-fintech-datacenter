354a7f77e3cb819759fc8685d084ae344e76c1b1
dumingcode <jake1036@126.com>
2019-03-05 18:52:14 +0800

    调整批处理任务

--- schedule/scheduleQuant.js
+++ schedule/scheduleQuant.js
@@ -3,12 +3,21 @@
  */
 var schedule = require('node-schedule');
 const stockAlphaBetaService = require('../service/quant/stockAlphaBetaService')
+const stockMa20DataService = require('../service/quant/stockMa20Service')
 const log = require('../util/logUtil')
 const logUtil = log.logUtil
 
 //计算个股相对沪深300的alpha 和 beta值
 schedule.scheduleJob('13 1 * * *', stockAlphaBetaService.launchStockAlphaBetaTask().then((val) => {
     logUtil.info({ val }, 'launchStockAlphaBetaTask success')
+}).catch((err) => {
+    logUtil.error(err)
+}))
+
+
+// 每天计算一次 转债的ma20
+schedule.scheduleJob('13 08 * * *', stockMa20DataService.launchStockMa20DailyDataTask().then((val) => {
+    logUtil.info({ val }, 'launchStockMa20DailyDataTask success')
 }).catch((err) => {
     logUtil.error(err)
 }))
