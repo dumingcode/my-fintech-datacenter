0afa45d4a7e993709bf3e056d3e42f111fbee3c7
dumingcode <jake1036@126.com>
2019-03-05 18:41:42 +0800

    添加ma20定时任务

--- schedule/scheduleLow.js
+++ schedule/scheduleLow.js
@@ -4,7 +4,7 @@ const log = require('../util/logUtil')
 const logUtil = log.logUtil
 
 //每日允许抓取股票历史价格数据
-schedule.scheduleJob('11 2,23 * * *', yearMinPriceService.launchStockDailyDataTask().then((val) => {
+schedule.scheduleJob('11 2,23,07 * * *', yearMinPriceService.launchStockDailyDataTask().then((val) => {
     logUtil.info({ val }, 'yearMinPriceService success')
 }).catch((err) => {
     logUtil.error(err)
--- schedule/scheduleWeek.js
+++ schedule/scheduleWeek.js
@@ -1,12 +1,20 @@
 var schedule = require('node-schedule');
 const stockWeeklyTask = require('../service/stockHisDataWeeklyService')
+const stockMa20DataService = require('../service/quant/stockMa20Service')
 const log = require('../util/logUtil')
 const logUtil = log.logUtil
 
 
-//改成每天跑一次同步15天内的股票交易价格
+//改成每天跑一次同步260天内的股票交易价格
 schedule.scheduleJob('13 23 * * *', stockWeeklyTask.launchStockHisDataWeekTask().then((val) => {
     logUtil.info({ val }, 'launchStockHisDataWeekTask success')
+}).catch((err) => {
+    logUtil.error(err)
+}))
+
+// 每天计算一次 转债的ma20
+schedule.scheduleJob('13 08 * * *', stockMa20DataService.launchStockMa20DailyDataTask().then((val) => {
+    logUtil.info({ val }, 'launchStockMa20DailyDataTask success')
 }).catch((err) => {
     logUtil.error(err)
 }))
