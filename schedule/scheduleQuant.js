50afc047a24d5443c97d0e8460db0df7f9272a3e
dumingcode <jake1036@126.com>
2019-03-05 18:58:36 +0800

    量化计算相关批处理任务集合到一个schedule

--- schedule/scheduleQuant.js
+++ schedule/scheduleQuant.js
@@ -4,6 +4,7 @@
 var schedule = require('node-schedule');
 const stockAlphaBetaService = require('../service/quant/stockAlphaBetaService')
 const stockMa20DataService = require('../service/quant/stockMa20Service')
+const yearMinPriceService = require('../service/process/yearMinPriceService')
 const log = require('../util/logUtil')
 const logUtil = log.logUtil
 
@@ -18,6 +19,13 @@ schedule.scheduleJob('13 1 * * *', stockAlphaBetaService.launchStockAlphaBetaTas
 // 每天计算一次 转债的ma20
 schedule.scheduleJob('13 06,08 * * *', stockMa20DataService.launchStockMa20DailyDataTask().then((val) => {
     logUtil.info({ val }, 'launchStockMa20DailyDataTask success')
+}).catch((err) => {
+    logUtil.error(err)
+}))
+
+// 52周最低价
+schedule.scheduleJob('11 2,23,07 * * *', yearMinPriceService.launchStockDailyDataTask().then((val) => {
+    logUtil.info({ val }, 'yearMinPriceService success')
 }).catch((err) => {
     logUtil.error(err)
 }))
