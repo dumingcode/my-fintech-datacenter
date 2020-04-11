e006d9e37e0a4970df328392ba67b856e0a75d0f
dumingcode <jake1036@126.com>
2020-04-11 11:43:51 +0800

    添加指数信息

--- service/lxrIndexService.js
+++ service/lxrIndexService.js
@@ -13,15 +13,33 @@ module.exports = {
         metricsList: config.lixingren.indexRetPara
       })
     if (lxrIndexData.status !== 200) return { status: lxrIndexData.status, message: lxrIndexData.statusText }
+
+    const lxrHkIndexData = await this.queryLxrApi(config.lixingrenHk.indexUrl,
+      {
+        token: config.lixingren.token,
+        date: 'latest',
+        stockCodes: config.lixingrenHk.stockIndex,
+        metricsList: config.lixingrenHk.indexRetPara
+      })
+    if (lxrHkIndexData.status !== 200) return { status: lxrHkIndexData.status, message: lxrHkIndexData.statusText }
+
+    const lxrUSAIndexData = await this.queryLxrApi(config.lixingrenUSA.indexUrl,
+      {
+        token: config.lixingren.token,
+        date: 'latest',
+        stockCodes: config.lixingrenUSA.stockIndex,
+        metricsList: config.lixingrenUSA.indexRetPara
+      })
+    if (lxrUSAIndexData.status !== 200) return { status: lxrUSAIndexData.status, message: lxrUSAIndexData.statusText }
+
     try {
       await this.saveLxrIndexData(lxrIndexData)
+      await this.saveLxrIndexData(lxrHkIndexData)
+      await this.saveLxrIndexData(lxrUSAIndexData)
     } catch (err) {
       console.log(err)
     }
     return { status: 200, message: 'OK' }
-  },
-  queryLxrApi (url, data) {
-    return http.post(url, data, false)
   },
   // 理性人指数数据redis持久化-
   async saveLxrIndexData (response) {
@@ -52,31 +70,56 @@ module.exports = {
     await redisUtil.redisSet(config.redisStoreKey.lxrIndexDealDateKey, tempLeastDealDate)
     await redisUtil.redisSet(config.redisStoreKey.lxrIndexDataAll, JSON.stringify(indexDataAll))
   },
+  queryLxrApi (url, data) {
+    return http.post(url, data, false)
+  },
   queryIndexName (code) {
     const index = {
-      399550: '央视50',
-      399395: '国证有色',
-      399998: '中证煤炭',
-      399393: '国证地产',
-      10002: '国企指数',
+      '399550': '央视50',
+      '399395': '国证有色',
+      '399998': '中证煤炭',
+      '399393': '国证地产',
+      '10002': '国企指数',
       '000015': '红利指数',
-      399005: '中小板指',
-      399006: '创业板',
-      399673: '创业板50',
-      399812: '养老产业',
-      399971: '中证传媒',
-      399975: '证券公司',
-      399986: '中证银行',
+      '399005': '中小板指',
+      '399006': '创业板',
+      '399673': '创业板50',
+      '399812': '养老产业',
+      '399971': '中证传媒',
+      '399975': '证券公司',
+      '399986': '中证银行',
       '000827': '中证环保',
       '000905': '中证500',
       '000922': '中证红利',
       '000925': '基本面50',
       '000978': '医药100',
       '000991': '全指医药',
-      10001: '恒生指数',
+      '10001': '恒生指数',
       '000300': '沪深300',
       '000932': '中证消费',
-      399701: '深证F60'
+      '399701': '深证F60',
+      '930782': '500SNLV',
+      '930846': '300SNLV', 
+      '931009': '建筑材料', 
+      '931087': '科技龙头', 
+      '931144': '通信技术', 
+      '931380': '科技50',
+      'H30094': '消费红利', 
+      'H30533': '中国互联网50', 
+      '000018': '180金融', 
+      '000807': '食品饮料', 
+      '000941': '新能源', 
+      '000979': '大宗商品', 
+      '399295': '创业蓝筹', 
+      '399809': '保险主题', 
+      '399995': '基建工程', 
+      '399997': '中证白酒',
+      '000992': '全指金融',
+      '.INX': '标普500',
+      'HSCAIT': '恒生A股行业龙头指数',
+      'HSCEI': '恒生中国企业指数',
+      'HSI': '恒生指数',
+      'HSMSI': '恒生综合中小型股指数'
     }
     let name = index[code]
     if (!name) {
